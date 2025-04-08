0b61000095e6f6b2de9a99c35eb24baadf1b2dbb
shubhamdubeyyyy <shubhamdubey754823@gmail.com>
2025-04-08 11:11:48 +0530

    List of Register Student

---
+++ Day6/frontend/src/components/Delete.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import axios from 'axios'
+const Delete = () => {
+    const handledelete = async (e) => {
+        
+        e.preventDefault();
+        const id = e.target.id.value;
+        await axios.delete(`http://localhost:9000/users/${id}`)
+        alert('User deleted Succussfully')
+    }
+  return (
+    <div>
+        <div>
+            <form onSubmit={handledelete}>
+                UserId<input type="text" placeholder="User ID" name="id" required/>
+                <button type="submit">Delete</button>
+            </form>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Delete
---
+++ Day6/frontend/src/components/Register.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import axios from 'axios'
+const Register = () => {
+    const handleregister = async (e)=>{
+        e.preventDefault()
+        const name = e.target.name.value;
+        const age = e.target.age.value;
+        const users={name,age}
+        await axios.post('http://localhost:9000/users',users)
+        alert('User registration Successfully')
+    }
+    return(
+        <div>
+            <form onSubmit={handleregister}>
+                Name<input type='text' placeholder='Name' name='name'required/>
+                Age<input type='number'placeholder='Age' name='age'required/>
+                <button type='submit'>Register</button>
+            </form>
+        </div>
+    )
+}
+
+export default Register
